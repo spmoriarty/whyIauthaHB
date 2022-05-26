5a1f6fcd9af9f78c90c1d3a4859d5e3b10b03cd0
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-26 10:41:54 -0700

    adding in redirects

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+import { redirectIfLoggedIn, signupUser, signInUser } from "./fetch-utils.js";
+
+
 const signInForm = document.getElementById('sign-in');
+const signInEmail = document.getElementById('sign-in-email');
+const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
+const signUpEmail = document.getElementById('sign-up-email');
+const signUpPassword = document.getElementById('sign-up-password');
+
+redirectIfLoggedIn();
+
+signUpForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const user = await signupUser(signUpEmail.value, signUpPassword.value);
+    return user;
+
+});
+
+signInForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const user = await signInUser(signInEmail.value, signInPassword.value);
+
+    if (user) {
+        redirectIfLoggedIn();
+    }
+});
+
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
+
+
+
